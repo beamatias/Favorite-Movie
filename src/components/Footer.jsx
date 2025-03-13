4cc705cec2c2eece05673e3aa0c01783608ea37d
beamatias <beatrizmathias2016@gmail.com>
2025-03-13 12:08:16 -0300

    Complete project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <p>Desenvolvido por Beatriz Matias</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiSearchAlt2 } from "react-icons/bi";
+
+import ImgLogo from "../../public/img/logoCine.png";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/" className="logo-link">
+          <img src={ImgLogo} alt="Logo" className="logo" />
+          Filmes Favoritos
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit} className="search-form">
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
